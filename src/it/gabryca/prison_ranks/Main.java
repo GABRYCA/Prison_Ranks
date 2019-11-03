6dc99699776b93e9825cb795e68630a64c26277d
GABRYCA <39743848+GABRYCA@users.noreply.github.com>
2019-11-03 16:32:42 +0100

    Main class fix

--- src/it/gabryca/prison_ranks/Main.java
+++ src/it/gabryca/prison_ranks/Main.java
@@ -44,7 +44,7 @@ public class Main extends JavaPlugin {
         getCommand("AddPrestige").setExecutor(new AddPrestige());
         getCommand("Ranks").setExecutor(new Ranks());
         getCommand("DelRank").setExecutor(new DelRank());
-        getCommand("DelPretige").setExecutor(new DelPrestige());
+        getCommand("DelPrestige").setExecutor(new DelPrestige());
         this.saveDefaultConfig();
         config = this;
         this.saveConfig();
